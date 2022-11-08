2d73785ed1b17a5c597c00c36cd8828b2c197d1a
s-121-06 <s-121-06@gumiforest.ae.krakow.pl>
2022-11-08 13:05:50 +0100

    During class
    Signed-off-by: s-121-06 <s-121-06@gumiforest.ae.krakow.pl>

---
+++ 04-DataEncapsulation/DC/test.java
@@ -0,0 +1,17 @@
+public class test {
+    
+    public static void main(String[] args) {
+        
+        DrivingLicense l1 = new DrivingLicense();
+        l1.nameAndSurname = "Jan Kowalski";
+        l1.address = "xyz";
+        l1.postalCode ="00-123";
+        l1.city = "Krakow";
+        l1.licenseNumber = "123456";
+        l1.yearOdIssue = "2022";
+        l1.licenseCategory = "X";
+
+        d1.display();
+    }
+
+}
